476e0a5ddf7a47e7607f12d5e4e3255616040284
abh-c <59344446+abh-c@users.noreply.github.com>
2020-09-01 11:49:54 -0400

    Update format.js

--- format.js
+++ format.js
@@ -2,7 +2,7 @@ function convert() {
     let txt = document.getElementById("text").value.split("\n");
     let str = "";
     
-    str += '<p style="padding: 0px; margin: 0px;">&lt;section class="section-1"&gt;</p>'
+    str += '<p style="padding: 0px; margin: 0px;">&lt;section class="code"&gt;</p>'
     str += '<p style="padding: 0px; margin: 0px;">&lt;pre&gt;</p>'
 
     for (let i = 0; i < txt.length; i++) {
