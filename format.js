c587db02c775208f557b808bab65e7163e164ea7
Abhishek Singh <abhishek.singh2@outlook.com>
2020-09-01 12:37:43 -0400

    Fix stuff

--- format.js
+++ format.js
@@ -1,12 +1,14 @@
 function convert() {
     let txt = document.getElementById("text").value.split("\n");
     let str = "";
+    let temp;
     
     str += '<p style="padding: 0px; margin: 0px;">&lt;section class="code"&gt;</p>'
     str += '<p style="padding: 0px; margin: 0px;">&lt;pre&gt;</p>'
 
     for (let i = 0; i < txt.length; i++) {
-        str += "<p style='padding: 0px; margin: 0px;'>&lt;code&gt;" + txt[i] + "&lt;/code&gt;</p>";
+        temp = txt[i].replace("<", "&lt;").replace(">", "&gt;");
+        str += "<pre style='padding: 0px; margin: 0px;'>&lt;code&gt;" + temp + "&lt;/code&gt;</pre>";
     }
 
     str += '<p style="padding: 0px; margin: 0px;">&lt;/pre&gt;</p>'
